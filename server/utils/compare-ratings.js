123418e7b0f389dd203d49736af10ba2e3c3f015
malak <malak.abogdaya@gmail.com>
2023-04-18 20:58:34 +0300

    compare mentors by the rating average

--- server/utils/compare-ratings.js
+++ server/utils/compare-ratings.js
@@ -1,15 +1,18 @@
 function compareSkillsRatings(mentor1, mentor2) {
     let mentor1Ratings = 0;
     let mentor2Ratings = 0;
-    let skillLength = mentor1.skills.length > mentor2.skills.length ? mentor1.skills.length : mentor2.skills.length
-    for (let i = 0; i < skillLength; i++) {
-        mentor1Ratings += mentor1.skills[i]?.rating ? mentor1.skills[i].rating : 0;
-        mentor2Ratings += mentor2.skills[i]?.rating ? mentor2.skills[i].rating : 0;
-    }
-    if (mentor1Ratings >= mentor2Ratings) {
+    mentor1.skills.forEach(skill => {
+        mentor1Ratings += skill.rating;
+    });
+    mentor2.skills.forEach(skill => {
+        mentor2Ratings += skill.rating;
+    });
+    let mentor1RatingsAvg = mentor1Ratings / (mentor1.skills.length * 5);
+    let mentor2RatingsAvg = mentor2Ratings / (mentor2.skills.length * 5);
+    if (mentor1RatingsAvg >= mentor2RatingsAvg) {
         return -1;
     }
-    return 1;
+    return 1
 }
 
 function compareSkillRating(mentor1, mentor2, skillName) {
