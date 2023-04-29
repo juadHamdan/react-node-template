361304dcb19b6fbce00d8ec842f9181e2e9eb06e
malak <malak.abogdaya@gmail.com>
2023-04-29 18:25:23 +0300

    company future meetings

--- client/src/components/company/Company.js
+++ client/src/components/company/Company.js
@@ -1,19 +1,20 @@
 import './company.css'
-import {useState, useEffect} from 'react'
-import {fetchCompanyPendingUsers, fetchCompanyUsers} from '../../CompanyApi'
-import {approveUser} from '../../MentorsApi'
+import { useState, useEffect } from 'react'
+import { fetchCompanyPendingUsers, fetchCompanyUsers, fetchFutureMeetings } from '../../CompanyApi'
+import { approveUser } from '../../MentorsApi'
+import CustomizedTables from "../../table/table"
 
-const Company = ({company}) => {
+const Company = ({ company }) => {
     console.log(company)
     const [companyUsers, setCompanyUsers] = useState([])
     const [companyPendingUsers, setCompanyPendingUsers] = useState([])
+    const [futureMeetings, setFutureMeetings] = useState([])
 
     useEffect(() => {
         //Promise.All concept
-        if(company){
+        if (company) {
             const getCompanyUsers = async () => {
                 const fetchedCompanyUsers = await fetchCompanyUsers(company._id)
-                //console.log()
                 setCompanyUsers(fetchedCompanyUsers)
                 const mentees = fetchedCompanyUsers.filter(user => !user.isMentor)
                 const mentors = fetchedCompanyUsers.filter(user => user.isMentor)
@@ -27,9 +28,14 @@ const Company = ({company}) => {
                 const numOfPendign = fetchedCompanyFetchedUsers.length
             }
             getCompanyPendingUsers()
+
+            const getFutureMeetings = async () => {
+                const fetchedMeetings = await fetchFutureMeetings(company._id)
+                setFutureMeetings(fetchedMeetings)
+            }
+            getFutureMeetings()
         }
     }, [])
-
 
     const onApproveUser = async (userId, index) => {
         await approveUser(userId)
@@ -37,14 +43,15 @@ const Company = ({company}) => {
         newPendingUsers.splice(index, 1)
         setCompanyPendingUsers(newPendingUsers)
     }
-
-  return (
-    <div>
-        <div>Display Company Data and allow for edit (and Add)</div>
-        {companyPendingUsers && companyPendingUsers.map((pendingUser, index) => 
-            <div onClick={() => onApproveUser(pendingUser._id, index)}>{pendingUser.firstName}</div>)}
-    </div>
-  )
+    console.log(futureMeetings);
+    return (
+        <div>
+            <div>Display Company Data and allow for edit (and Add)</div>
+            {companyPendingUsers && companyPendingUsers.map((pendingUser, index) =>
+                <div onClick={() => onApproveUser(pendingUser._id, index)}>{pendingUser.firstName}</div>)}
+            <CustomizedTables meetings={futureMeetings} />
+        </div>
+    )
 }
 
 export default Company
---
+++ server/utils/time-formate.js
@@ -0,0 +1,7 @@
+const formatTime = (hours, minutes) => {
+    let formatedHoure = hours > 9 ? hours : "0" + hours;
+    let formatedMinutes = minutes > 9 ? minutes : "0" + minutes;
+    return formatedHoure + " : " + formatedMinutes;
+}
+
+module.exports = { formatTime }
