2570f908fe02116a9b3b6bbc0750abf65a74fc59
narmeen <narmeen.kh99@gmail.com>
2023-04-30 11:00:12 +0300

    pending page

---
+++ client/src/components/pending-page/PandingMessage.js
@@ -0,0 +1,11 @@
+import "./pending-page.css";
+
+const PandingMessage = () => {
+  return (
+    <div className="pending-message">
+      <h3>Please wait for company approval.</h3>
+    </div>
+  );
+};
+
+export default PandingMessage;
--- client/src/components/pending-page/PendingPage.js
+++ client/src/components/pending-page/PendingPage.js
@@ -1,27 +1,44 @@
-import { useState, useEffect } from 'react'
-import { fetchCompanies, addPendingUserToCompany } from '../../CompanyApi'
+import "./pending-page.css";
+import { useState, useEffect } from "react";
+import { fetchCompanies, addPendingUserToCompany } from "../../CompanyApi";
+import PandingMessage from "./PandingMessage";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
 
-const PendingPage = ({user}) => {
-    const [companies, setCompanies] = useState([])
+const PendingPage = ({ user }) => {
+  const [companies, setCompanies] = useState([]);
+  const [showPendingMessage, setShowPendingMessage] = useState(false);
 
-    useEffect(() => {
-        const getCompanies = async ()  => {
-            const fetchedCompanies = await fetchCompanies()
-            setCompanies(fetchedCompanies)
-        }
-        getCompanies()
-    }, [])
+  useEffect(() => {
+    const getCompanies = async () => {
+      const fetchedCompanies = await fetchCompanies();
+      setCompanies(fetchedCompanies);
+    };
+    getCompanies();
+  }, []);
 
-    const onCompanySubmit = async (companyId) => {
-        await addPendingUserToCompany(companyId, user._id)
-        alert("Requested To Join Company.")
-    }
+  const onCompanySubmit = async (companyId) => {
+    await addPendingUserToCompany(companyId, user._id);
+    setShowPendingMessage(true);
+  };
 
-    return (
-        <div>
-            {companies && companies.map(company => <button onClick={() => onCompanySubmit(company._id)}>{company.name}</button>)}
-        </div>
-    )
-}
+  return (
+    <div id="pending-page-container">
+      {companies && (
+        <Autocomplete
+          options={companies}
+          sx={{ width: 300 }}
+          onChange={(e, option) => onCompanySubmit(option._id)}
+          getOptionLabel={(option) => option.name}
+          renderInput={(params) => (
+            <TextField {...params} label="Choose A Company" />
+          )}
+        />
+      )}
 
-export default PendingPage
+      {showPendingMessage && <PandingMessage />}
+    </div>
+  );
+};
+
+export default PendingPage;
